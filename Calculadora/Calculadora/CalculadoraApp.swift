f5fa4a73ff104a7ad12c4fd7825108bb27e0aa2a
DevSaLLein <devzac.andrade@gmail.com>
2025-02-14 14:10:29 -0300

    initial calculator swift project

---
+++ Calculadora/Calculadora/CalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraApp.swift
+//  Calculadora
+//
+//  Created by iredefbmac_22 on 14/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculadora/Calculadora/ContentView.swift
@@ -0,0 +1,214 @@
+//
+//  ContentView.swift
+//  Calculadora
+//
+//  Created by iredefbmac_22 on 13/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var result = 0
+    
+    var body: some View {
+        
+        VStack(alignment: .trailing,  spacing: 0) {
+            
+            Spacer()
+            
+            HStack {
+                
+                Text(String(result))
+                    .padding()
+                    .font(.system(size: CGFloat(80 / Int((Double(String(result).count) + 10) / 8.0))))
+                    .lineLimit(1)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .fixedSize(horizontal: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, vertical: false)
+            }
+            
+            HStack {
+                
+                Button("AC") {
+                    
+                    result = 0
+                    
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+                Button("+/-") {
+                    
+            
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+    
+                Button("%") {
+                    
+                    
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+                Button("/") {
+                    
+                    
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color.orange)
+            
+                
+            }
+            .foregroundStyle(Color.white)
+            
+            HStack {
+                
+                Button("7") {
+                    
+                    
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+                Button("8") {
+                    
+            
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+    
+                Button("9") {
+                    
+                    
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+                Button("X") {
+                    
+                    
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color.orange)
+            
+                
+            }
+            .foregroundStyle(Color.white)
+            
+            HStack {
+                
+                Button("4") {
+                    
+                    
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+                Button("5") {
+                    
+            
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+    
+                Button("6") {
+                    
+                    
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+                Button("-") {
+                    
+                    
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color.orange)
+            
+                
+            }
+            .foregroundStyle(Color.white)
+
+            HStack {
+                
+                Button("1") {
+                    
+                   result = (result * 10) + 1
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+                Button("2") {
+                    
+                    result = (result * 10) + 2
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+    
+                Button("3") {
+                    
+                    
+                }
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+                Button("+") {
+                        
+                    
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color.orange)
+            }
+            
+            .foregroundStyle(Color.white)
+            
+            
+            GeometryReader { geometry in
+                
+                HStack {
+                    
+                    Button("0") {
+                        
+                        
+                    }
+                    .padding()
+                    .frame(minWidth: geometry.size.width / 2)
+                    
+                    Button(",") {
+                        
+                        
+                    }
+                    .padding()
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    
+                    Button("=") {
+                        
+                        
+                    }
+                    .padding(.vertical, 40)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .background(Color.orange)
+                    
+                }
+                .foregroundStyle(Color.white)
+                   
+            }
+            .frame(maxHeight: 92)
+
+            
+        }
+        
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+    }
+}
+
+#Preview {
+    ContentView()
+}
